5de6b3d126df5e64a486d64c3ad59ed79149e40c
Mikael Melkonyan <melkonyan2311@mail.ru>
2016-11-09 18:49:00 +0200

    Project creation

---
+++ Sky Well Test Project/Sky Well Test Project/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Sky Well Test Project
+//
+//  Created by miki on 11/9/16.
+//  Copyright © 2016 JesusCodes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
